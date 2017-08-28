1018fb96209ea40205b6bb18325493f1eec2860a
DavidChindo <dbarrera.chindo@gmail.com>
2017-08-27 20:02:44 -0500

    preDesing init

--- Biofields/app/src/main/java/com/hics/biofields/Views/Activity/MainActivity.java
+++ Biofields/app/src/main/java/com/hics/biofields/Views/Activity/MainActivity.java
@@ -3,18 +3,25 @@ package com.hics.biofields.Views.Activity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.hics.biofields.R;
 import com.hics.biofields.Views.Adapters.RequisitionAdapter;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import in.galaxyofandroid.awesometablayout.AwesomeTabBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        toolbar.setTitle("Requsiciones");
         AwesomeTabBar tabBar=(AwesomeTabBar)findViewById(R.id.tabBar);
         ViewPager pager=(ViewPager)findViewById(R.id.pager);
         pager.setAdapter(new RequisitionAdapter(getSupportFragmentManager()));
--- Biofields/app/src/main/java/com/hics/biofields/Views/Activity/SplashActivity.java
+++ Biofields/app/src/main/java/com/hics/biofields/Views/Activity/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.hics.biofields.BuildConfig;
